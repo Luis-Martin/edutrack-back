7e6de70df0de77af48c327e63ffc5ed9aefe4deb
Martin <luis.aaron.mp17@gmail.com>
2025-06-11 15:04:13 -0500

    Agregar sstudent y professor al panel del administrador

--- professor/admin.py
+++ professor/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Professor
 
-# Register your models here.
+@admin.register(Professor)
+class ProfessorAdmin(admin.ModelAdmin):
+    list_display = ('id_professor', 'first_name', 'last_name', 'email', 'phone', 'created_at')
+    search_fields = ('id_professor', 'first_name', 'last_name', 'email')
+    list_filter = ('created_at',)
--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Student
 
-# Register your models here.
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id_student', 'first_name', 'last_name', 'email', 'phone', 'created_at')
+    search_fields = ('id_student', 'first_name', 'last_name', 'email')
+    list_filter = ('created_at',)
